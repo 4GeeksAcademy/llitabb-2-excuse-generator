a1bb5a685cbe471d7842952aa802d497185ce9c2
Llitabb <156102986+Llitabb@users.noreply.github.com>
2024-02-29 18:27:28 +0000

    Final con comentarios y boton

--- src/app.js
+++ src/app.js
@@ -2,11 +2,9 @@
 import "bootstrap";
 import "./style.css";
 
-//write your code here
-//primera frase
-//abro función y
-
+// Función que generará excusas aleatorias
 function generadorExcusas() {
+  // Arrays que generarán distintas partes de la oración de la excusa
   let quien = ["Mi gato", "Mi sobrino", "Una cigüeña", "El perro del vecino"];
   let accion = [
     "lanzó pintura sobre",
@@ -16,17 +14,26 @@ function generadorExcusas() {
   ];
   let que = ["mi portatil", "mi coche", "mis llaves"];
   let cuando = [
-    "hace una hora",
+    "hace una hora.",
     "mientras me vestía para salir.",
     "cuando me ataba los cordones.",
     "mientras tendía la ropa."
   ];
 
-  let quienId = Math.floor(Math.random() * quien.length);
-  let accionId = Math.floor(Math.random() * accion.length);
-  let queID = Math.floor(Math.random() * que.length);
-  let cuandoId = Math.floor(Math.random() * cuando.length);
+  //Función que generará un valor random según el largo del array
 
+  const numeroRandom = myArray => {
+    return Math.floor(Math.random() * myArray.length);
+  };
+
+  // Variables que en las que llamamos a la función numeroRandom para generar el valor random que luego nos retornará la función generadorExcusas
+
+  let quienId = numeroRandom(quien);
+  let accionId = numeroRandom(accion);
+  let queID = numeroRandom(que);
+  let cuandoId = numeroRandom(cuando);
+
+  // Composición de retorno para la excusa generada concatenando los valores generados aleatoriamente
   return (
     quien[quienId] +
     " " +
@@ -38,6 +45,15 @@ function generadorExcusas() {
   );
 }
 
+// Se ejecutará cuando se carga la ventana
+
 window.onload = function() {
+  // Se asigna el resultado de la función generadorExcusas al elemento con id "excusa" en el HTML
   document.getElementById("excusa").innerHTML = generadorExcusas();
+  //Se asigna llamada al HTML de botón refrescar
+  document
+    .getElementById("botonRefrescar")
+    .addEventListener("click", function() {
+      location.reload();
+    });
 };
