bec40c1a7ddd0b0a83a6e9c088c5251702efd332
Llitabb <156102986+Llitabb@users.noreply.github.com>
2024-03-06 19:17:16 +0000

    Standard edition v.2

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@
 import "bootstrap";
 import "./style.css";
 
-// Función what generará excusas aleatorias
+// Función que generará excusas aleatorias
 function excuseGenerator() {
-  // Arrays what generarán distintas partes de la oración de la excusa
+  // Arrays que generarán distintas partes de la oración de la excusa
   let who = ["Mi gato", "Mi sobrino", "Una cigüeña", "El perro del vecino"];
   let action = [
     "lanzó pintura sobre",
@@ -20,33 +20,32 @@ function excuseGenerator() {
     "mientras tendía la ropa."
   ];
 
-  //Función what generará un valor random según el largo del array
+  //Función que generará un valor random según el largo del array
 
   const randomNumber = myArray => {
     return Math.floor(Math.random() * myArray.length);
   };
 
-  // Variables what en las what llamamos a la función randomNumber para generar el valor random what luego nos retornará la función excuseGenerator
+  // Variables que en las que llamamos a la función randomNumber para generar el valor random que luego nos retornará la función excuseGenerator
   let whoId = randomNumber(who);
   let accionId = randomNumber(action);
   let whatID = randomNumber(what);
   let whenId = randomNumber(when);
 
   // Composición de retorno para la excusa generada concatenando los valores generados aleatoriamente
-  /* PRIMER RETURN what FUNCIONA:  return (
+  /* PRIMER RETURN what FUNCIONA:  
+  return (
     who[whoId] +
     " " +
     action[accionId] +
     " " +
     what[whatID] +
     " " +
-    when
-[when
-  Id]
+    when[whenId]
   );
 }*/
 
-  //SEGUNDO RETURN what FUNCIONA (más claro y limpio)
+  //SEGUNDO RETURN QUE FUNCIONA (más claro y limpio)
   return `${who[whoId]} ${action[accionId]} ${what[whatID]} ${when[whenId]}`;
 }
 
@@ -58,12 +57,12 @@ window.onload = function() {
   //Se asigna llamada al HTML de botón refrescar
   /* PRIMERA OPCIÓN (de esta forma se recarga toda la página):
    document
-    .getElementById("botonRefrescar")
+    .getElementById("refreshButton")
     .addEventListener("click", function() {
       location.reload();
     });
 */
-  // SEGUNDA OPCIÓN (de esta forma no se recarga toda la página):
+  // SEGUNDA OPCIÓN (de esta forma NO se recarga toda la página):
   document.getElementById("refreshButton").addEventListener("click", () => {
     document.getElementById("excuse").innerHTML = excuseGenerator();
   });
