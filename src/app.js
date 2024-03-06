495656f19aa3fb38d5b5e7d7f966d35fad2c88bf
Llitabb <156102986+Llitabb@users.noreply.github.com>
2024-03-06 19:14:03 +0000

    Standard edition

--- src/app.js
+++ src/app.js
@@ -2,58 +2,59 @@
 import "bootstrap";
 import "./style.css";
 
-// Función que generará excusas aleatorias
-function generadorExcusas() {
-  // Arrays que generarán distintas partes de la oración de la excusa
-  let quien = ["Mi gato", "Mi sobrino", "Una cigüeña", "El perro del vecino"];
-  let accion = [
+// Función what generará excusas aleatorias
+function excuseGenerator() {
+  // Arrays what generarán distintas partes de la oración de la excusa
+  let who = ["Mi gato", "Mi sobrino", "Una cigüeña", "El perro del vecino"];
+  let action = [
     "lanzó pintura sobre",
     "derramó gasolina y una cerilla encendida en",
     "se comió",
     "rompió"
   ];
-  let que = ["mi portatil", "mi coche", "mis llaves"];
-  let cuando = [
+  let what = ["mi portatil", "mi coche", "mis llaves"];
+  let when = [
     "hace una hora.",
     "mientras me vestía para salir.",
     "cuando me ataba los cordones.",
     "mientras tendía la ropa."
   ];
 
-  //Función que generará un valor random según el largo del array
+  //Función what generará un valor random según el largo del array
 
-  const numeroRandom = myArray => {
+  const randomNumber = myArray => {
     return Math.floor(Math.random() * myArray.length);
   };
 
-  // Variables que en las que llamamos a la función numeroRandom para generar el valor random que luego nos retornará la función generadorExcusas
-
-  let quienId = numeroRandom(quien);
-  let accionId = numeroRandom(accion);
-  let queID = numeroRandom(que);
-  let cuandoId = numeroRandom(cuando);
+  // Variables what en las what llamamos a la función randomNumber para generar el valor random what luego nos retornará la función excuseGenerator
+  let whoId = randomNumber(who);
+  let accionId = randomNumber(action);
+  let whatID = randomNumber(what);
+  let whenId = randomNumber(when);
 
   // Composición de retorno para la excusa generada concatenando los valores generados aleatoriamente
-  /* PRIMER RETURN QUE FUNCIONA:  return (
-    quien[quienId] +
+  /* PRIMER RETURN what FUNCIONA:  return (
+    who[whoId] +
     " " +
-    accion[accionId] +
+    action[accionId] +
     " " +
-    que[queID] +
+    what[whatID] +
     " " +
-    cuando[cuandoId]
+    when
+[when
+  Id]
   );
 }*/
 
-  //SEGUNDO RETURN QUE FUNCIONA (más claro y limpio)
-  return `${quien[quienId]} ${accion[accionId]} ${que[queID]} ${cuando[cuandoId]}`;
+  //SEGUNDO RETURN what FUNCIONA (más claro y limpio)
+  return `${who[whoId]} ${action[accionId]} ${what[whatID]} ${when[whenId]}`;
 }
 
-// Se ejecutará cuando se carga la ventana
+// Se ejecutará whe se carga la ventana
 
 window.onload = function() {
-  // Se asigna el resultado de la función generadorExcusas al elemento con id "excusa" en el HTML
-  document.getElementById("excusa").innerHTML = generadorExcusas();
+  // Se asigna el resultado de la función excuseGenerator al elemento con id "excusa" en el HTML
+  document.getElementById("excuse").innerHTML = excuseGenerator();
   //Se asigna llamada al HTML de botón refrescar
   /* PRIMERA OPCIÓN (de esta forma se recarga toda la página):
    document
@@ -63,7 +64,7 @@ window.onload = function() {
     });
 */
   // SEGUNDA OPCIÓN (de esta forma no se recarga toda la página):
-  document.getElementById("botonRefrescar").addEventListener("click", () => {
-    document.getElementById("excusa").innerHTML = generadorExcusas();
+  document.getElementById("refreshButton").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = excuseGenerator();
   });
 };
